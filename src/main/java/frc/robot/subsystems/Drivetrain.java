6d7d3af7ac9e21d6014ec14d20cef73d48b31c74
Donger1234 <dgkim2024@riordanhs.org>
2022-11-18 10:35:06 -0800

    drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,6 +7,24 @@ import frc.robot.RobotMap;
 import frc.robot.commands.archadedrive;
 public class Drivetrain extends Subsystem {
     // Put methods for controlling this subsystem here. Call these from Commands.
+    WPI_TalonFX l1, l2, r1, r2; 
+    MotorControllerGroup l, r;
+    public Drivetrain() {
+      l1 = new WPI_TalonFX(Constants.MOTOR_L1_ID);
+      l2 = new WPI_TalonFX(Constants.MOTOR_L2_ID);
+      r1 = new WPI_TalonFX(Constants.MOTOR_R1_ID);
+      r2 = new WPI_TalonFX(Constants.MOTOR_R1_ID); 
+      
+      r1.setInverted(true);
+      r2.setInverted(true);
+
+      l2.follow(l1);
+      r2.follow(r1);
+
+      l = new MotorControllerGroup(l1, l2);
+      r = new MotorControllerGroup(r1, r2);
+      ddrive = new DifferentialDrive(l, r);
+    }
     @Override
     public void initDefaultCommand() {
       // Set the default command for a subsystem here.
